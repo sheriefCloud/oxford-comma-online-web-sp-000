4a73b4bfb489f797c32966df0277da5022f00739
Sherief Farid <sherief@farid.cloud>
2019-03-02 07:08:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+require'pry'
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size ==2
+    array.join(" and ")
+  else
+    last_element = array.last
+    array.pop
+    array.join(", ") << ", and " << last_element
+# binding.pry
+  end
 end
